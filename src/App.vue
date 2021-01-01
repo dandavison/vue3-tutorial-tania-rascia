8fd94c0ea11965defc15ca0c9fd738391673ab30
Dan Davison <dandavison7@gmail.com>
2021-01-01 11:34:19 -0500

    https://www.taniarascia.com/getting-started-with-vue/
    with modifications for vue3

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app-component"></div>
+  <h1>Employees</h1>
+  <employee-table />
+</template>
+
+<script>
+import EmployeeTable from "./EmployeeTable.vue";
+export default {
+  components: { EmployeeTable },
+};
+</script>
+
+<style>
+EmployeeTable button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
