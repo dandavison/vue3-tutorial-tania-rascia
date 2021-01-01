5ac5fda58297b65f0d1a58d5e9e28278e4648996
Dan Davison <dandavison7@gmail.com>
2021-01-01 12:14:56 -0500

    Switch table data source from HTML to JS

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app-component"></div>
   <h1>Employees</h1>
-  <employee-table />
+  <employee-table v-bind:employees="employees" />
 </template>
 
 <script>
 import EmployeeTable from "./EmployeeTable.vue";
 export default {
+  setup() {
+    return {
+      employees: [
+        { name: "Richard Hendricks", email: "richard@piedpiper.com", id: 1 },
+        { name: "Bertram Gilfoyle", email: "gilfoyle@piedpiper.com", id: 2 },
+        { name: "Dinesh Chugtai", email: "dinesh@piedpiper.com", id: 3 },
+      ],
+    };
+  },
   components: { EmployeeTable },
 };
 </script>
