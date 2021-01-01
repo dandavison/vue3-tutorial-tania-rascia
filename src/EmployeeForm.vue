1e70a5ba600c68290617ed7b9322a8d533e09cb5
Dan Davison <dandavison7@gmail.com>
2021-01-01 14:53:34 -0500

    Form validation

--- src/EmployeeForm.vue
+++ src/EmployeeForm.vue
@@ -6,27 +6,76 @@
     <input v-model="employee.email" type="text" />
     <button>Add Employee</button>
   </form>
+  <div v-if="error && submitting" class="error-message">
+    ❗Please fill out all required fields
+  </div>
+  <div v-if="success" class="success-message">
+    ✅ Employee successfully added
+  </div>
 </template>
 
 <script>
+import { computed, ref } from "vue";
 export default {
   setup(props, { emit }) {
-    const employee = { name: "", email: "" };
+    let employee = { name: "", email: "" };
+    let submitting = ref(false);
+    let error = ref(false);
+    let success = ref(false);
+
+    const invalidName = computed(() => employee.name === "");
+    const invalidEmail = computed(() => employee.email === "");
+
+    const clearStatus = () => {
+      success.value = false;
+      error.value = false;
+    };
+
+    const handleSubmit = () => {
+      submitting.value = true;
+      clearStatus();
+      if (employee.name === "" || employee.email == "") {
+        // FIXME: use computed properties
+        error.value = true;
+        return;
+      }
+
+      fetch({ method: "POST", url: "/employee/create" }).then(() => {
+        console.log("tried to POST employee data");
+        emit("add:employee", employee);
+        employee = { name: "", email: "" };
+        error.value = false;
+        submitting.value = false;
+        success.value = true;
+      });
+    };
+
     return {
       employee,
-      handleSubmit: () => {
-        fetch({ method: "POST", url: "/employee/create" }).then(() => {
-          console.log("tried to POST employee data");
-        });
-        emit("add:employee", employee);
-      },
+      handleSubmit,
+      submitting,
+      error,
+      success,
     };
   },
+  emits: ["add:employee"],
 };
 </script>
 
 <style scoped>
 form {
   margin-bottom: 2rem;
+}
+
+[class*="-message"] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
 }
 </style>
