ea67113389a6476c3df17769d197dbf217644cdb
Dan Davison <dandavison7@gmail.com>
2021-01-01 13:48:06 -0500

    Add form component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="app-component"></div>
-  <h1>Employees</h1>
-  <employee-table v-bind:employees="employees" />
+  <div id="app-component" class="small-container">
+    <employee-form @add:employee="addEmployee" />
+    <h1>Employees</h1>
+    <employee-table v-bind:employees="employees" />
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import EmployeeForm from "./EmployeeForm.vue";
 import EmployeeTable from "./EmployeeTable.vue";
 export default {
   setup() {
+    const employees = ref([
+      { name: "Richard Hendricks", email: "richard@piedpiper.com", id: 1 },
+      { name: "Bertram Gilfoyle", email: "gilfoyle@piedpiper.com", id: 2 },
+      { name: "Dinesh Chugtai", email: "dinesh@piedpiper.com", id: 3 },
+    ]);
+    const addEmployee = (employee) => {
+      console.log(`addEmployee: ${employee.name} ${employee.email}`);
+      const nextId = employees.value.length + 1;
+      employees.value.push({ ...employee, nextId });
+    };
     return {
-      employees: [
-        { name: "Richard Hendricks", email: "richard@piedpiper.com", id: 1 },
-        { name: "Bertram Gilfoyle", email: "gilfoyle@piedpiper.com", id: 2 },
-        { name: "Dinesh Chugtai", email: "dinesh@piedpiper.com", id: 3 },
-      ],
+      employees,
+      addEmployee,
     };
   },
-  components: { EmployeeTable },
+  components: { EmployeeTable, EmployeeForm },
 };
 </script>
 
---
+++ src/EmployeeForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit" id="employee-form">
+    <label>Name</label>
+    <input v-model="employee.name" type="text" />
+    <label>Email</label>
+    <input v-model="employee.email" type="text" />
+    <button>Add Employee</button>
+  </form>
+</template>
+
+<script>
+export default {
+  setup(props, { emit }) {
+    const employee = { name: "", email: "" };
+    return {
+      employee,
+      handleSubmit: () => {
+        fetch({ method: "POST", url: "/employee/create" }).then(() => {
+          console.log("tried to POST employee data");
+        });
+        emit("add:employee", employee);
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2rem;
+}
+</style>
