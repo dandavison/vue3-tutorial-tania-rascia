d20fae0a54e8ff82fad26aeb1ca18562796a8ddb
Dan Davison <dandavison7@gmail.com>
2021-01-01 15:29:49 -0500

    Fix form behavior: use ref

--- src/EmployeeForm.vue
+++ src/EmployeeForm.vue
@@ -28,13 +28,13 @@
 import { computed, ref } from "vue";
 export default {
   setup(props, { emit }) {
-    let employee = { name: "", email: "" };
+    let employee = ref({ name: "", email: "" });
     let submitting = ref(false);
     let error = ref(false);
     let success = ref(false);
 
-    const invalidName = computed(() => employee.name === "");
-    const invalidEmail = computed(() => employee.email === "");
+    const invalidName = computed(() => employee.value.name === "");
+    const invalidEmail = computed(() => employee.value.email === "");
 
     const clearStatus = () => {
       success.value = false;
@@ -44,16 +44,15 @@ export default {
     const handleSubmit = () => {
       submitting.value = true;
       clearStatus();
-      if (employee.name === "" || employee.email == "") {
-        // FIXME: use computed properties
+      if (employee.value.name === "" || employee.value.email == "") {
         error.value = true;
         return;
       }
 
       fetch({ method: "POST", url: "/employee/create" }).then(() => {
         console.log("tried to POST employee data");
-        emit("add:employee", employee);
-        employee = { name: "", email: "" };
+        emit("add:employee", employee.value);
+        employee.value = { name: "", email: "" };
         error.value = false;
         submitting.value = false;
         success.value = true;
